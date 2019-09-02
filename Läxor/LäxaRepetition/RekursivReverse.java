6efffe07bfe9fea368a74957145f26092048ea4a
augustadahl <august.adahl@outlook.com>
2019-09-02 10:44:19 +0200

    löst uppg 2

---
+++ Läxor/LäxaRepetition/RekursivReverse.java
@@ -0,0 +1,17 @@
+package LäxaRepetition;
+
+public class RekursivReverse {
+
+	public static void main(String[] args) {
+		System.out.println(reverse("Hasse"));
+	}
+	
+	public static String reverse(String Word) {
+		if (Word.length() == 1) {
+			return Word;
+		} else {
+			return Word.charAt(Word.length() - 1) + reverse(Word.substring(0, Word.length() - 1));
+		}
+	}
+	
+}
