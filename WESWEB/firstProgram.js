54829e899eb7a1e7cb07e54c0cae5d96653c4e2a
augustadahl <august.adahl@outlook.com>
2020-05-19 13:28:49 +0200

    s

--- WESWEB/firstProgram.js
+++ WESWEB/firstProgram.js
@@ -5,6 +5,10 @@ const port = 3000
 var clientDir = __dirname + '\\client\\'
 
 app.get('/', (req, res) => {
-  res.sendFile(clientDir + 'index.html')
+  res.sendFile(clientDir + 'css3_inlämning.html')
 })
+app.get('/shit', (req, res) => {
+  res.sendFile(clientDir + 'css3_inlämning.css')
+})
+
 app.listen(port, () => console.log(`Example app listening on port port!`))
